63f68d1fe0a2bdf3017000b3ce7a763afedaca27
Alex <2415735726@qq.com>
2025-04-08 15:44:02 +0800

    test.py 程序完成，可以分析md文件内容并存入向量库

--- kea/test.py
+++ kea/test.py
@@ -2,15 +2,50 @@ from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_ollama import OllamaEmbeddings
+from langchain_ollama import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 
-loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+# loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+loader = WebBaseLoader("https://raw.githubusercontent.com/openatx/uiautomator2/master/README_CN.md")
 data = loader.load()
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 all_splits = text_splitter.split_documents(data)
 local_embeddings = OllamaEmbeddings(model="nomic-embed-text")
 vectorstore = Chroma.from_documents(documents=all_splits, embedding=local_embeddings)
 
-question = "What are the approaches to Task Decomposition?"
+# question = "What are the approaches to uiautomator2?"
+# docs = vectorstore.similarity_search(question)
+# print(len(docs))
+# print(docs[3])
+
+model = ChatOllama(
+    model="llama3.1:8b",
+)
+
+# response_message = model.invoke(
+#     "Simulate a rap battle between Stephen Colbert and John Oliver"
+# )
+#
+# print(response_message.content)
+
+
+prompt = ChatPromptTemplate.from_template(
+    "Summarize the main themes in these retrieved docs: {docs}"
+)
+
+
+# Convert loaded documents into strings by concatenating their content
+# and ignoring metadata
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+chain = {"docs": format_docs} | prompt | model | StrOutputParser()
+
+question = "What are the approaches to uiautomator2?"
+
 docs = vectorstore.similarity_search(question)
-len(docs)
+
+print(chain.invoke(docs))
